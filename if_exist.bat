2ee84f8301018c5a1b4b7dbaf97896dc74ae100b
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-01-04 07:13:54 +0100

    + if_exist.bat

---
+++ if_exist.bat
@@ -0,0 +1,11 @@
+@if exist if_exist.bat (
+ echo The file exists
+) else (
+ echo The file does not exists
+)
+
+@if exist foo.bar (
+ echo The file exists
+) else (
+ echo The file does not exists
+)
