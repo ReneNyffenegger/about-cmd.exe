5ee9cdf52c03e37870f7d29f0ba55e053e8b4c1c
Rene Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2017-01-25 17:38:51 +0100

    + if_else_if_else.bat

---
+++ if_else_if_else.bat
@@ -0,0 +1,9 @@
+@if exist does_not_exist.bat (
+  echo "Unexpected does_not_exist.bat exists!"
+) else if exist nothing.bat (
+  echo "Unexpected nothing.bat.!"
+) else if exist if_else_if_else.bat (
+  echo "This was expected: if_else_if_else.bat exists"
+) else (
+  echo "None of these exists!:
+)
