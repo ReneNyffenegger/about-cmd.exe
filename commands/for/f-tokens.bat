79500ba2483745bf98cb6e1e4f9b8371ebaebb86
René Nyffenegger <mail@renenyffenegger.ch>
2019-01-22 08:08:46 +0100

    + f-tokens.bat

---
+++ commands/for/f-tokens.bat
@@ -0,0 +1,59 @@
+@set text=one   two  three four five    six seven
+
+@for /f "tokens=*" %%A in ("%text%") do @(
+  @echo A = %%A
+  @echo B = %%B
+  @echo C = %%C
+  @echo D = %%D
+  @echo E = %%E
+)
+
+
+@for /f "tokens=3*" %%A in ("%text%") do @(
+  @echo A = %%A
+  @echo B = %%B
+  @echo C = %%C
+  @echo D = %%D
+  @echo E = %%E
+)
+
+@for /f "tokens=1-4*" %%A in ("%text%") do @(
+  @echo A = %%A
+  @echo B = %%B
+  @echo C = %%C
+  @echo D = %%D
+  @echo E = %%E
+)
+
+
+@for /f "tokens=1-4,*" %%A in ("%text%") do @(
+  @echo A = %%A
+  @echo B = %%B
+  @echo C = %%C
+  @echo D = %%D
+  @echo E = %%E
+)
+
+@for /f "tokens=2,4,5" %%A in ("%text%") do @(
+  @echo A = %%A
+  @echo B = %%B
+  @echo C = %%C
+  @echo D = %%D
+  @echo E = %%E
+)
+
+@for /f "tokens=2,4,5*" %%A in ("%text%") do @(
+  @echo A = %%A
+  @echo B = %%B
+  @echo C = %%C
+  @echo D = %%D
+  @echo E = %%E
+)
+
+@for /f "tokens=2,4,5,*" %%A in ("%text%") do @(
+  @echo A = %%A
+  @echo B = %%B
+  @echo C = %%C
+  @echo D = %%D
+  @echo E = %%E
+)
