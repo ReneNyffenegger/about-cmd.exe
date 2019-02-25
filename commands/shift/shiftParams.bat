c23f32adb88a2de5db61b788a7f47c27943e5cff
René Nyffenegger <mail@renenyffenegger.ch>
2019-02-25 10:25:19 +0100

    + shiftParams.bat

---
+++ commands/shift/shiftParams.bat
@@ -0,0 +1,19 @@
+setlocal enableExtensions
+
+@echo param 1 = %1
+@echo param 2 = %2
+@echo param 3 = %3
+
+@echo now shifting parameters by one
+@shift
+
+@echo param 1 = %1
+@echo param 2 = %2
+@echo param 3 = %3
+
+@echo now shifting parameters by one
+@shift
+
+@echo param 1 = %1
+@echo param 2 = %2
+@echo param 3 = %3
