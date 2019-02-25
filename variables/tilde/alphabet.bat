76bc9d5e1922f6ff020e451e52a1c5e853c1a1d0
René Nyffenegger <mail@renenyffenegger.ch>
2019-02-25 11:43:03 +0100

    typo

--- variables/tilde/alphabet.bat
+++ variables/tilde/alphabet.bat
@@ -5,5 +5,5 @@ set tq84_var=abcdefghijklmnopqrstuvwxyz
 set without_first_five_characters=%tq84_var:~5%
 set last_five_characters=%tq84_var:~-5%
 
-echo Alphabeth without first five characters: %without_first_five_characters%
-echo Last five characters of alphabeth:       %last_five_characters%
+echo Alphabet without first five characters: %without_first_five_characters%
+echo Last five characters of alphabet:       %last_five_characters%
