4fcc18c89db6d19d4f43e991cd6df21e1fefd5d4
René Nyffenegger <mail@renenyffenegger.ch>
2019-02-25 13:03:53 +0100

    + pathExpansion.bat

---
+++ variables/tilde/pathExpansion.bat
@@ -0,0 +1,14 @@
+@echo off
+
+
+echo %%0                       %0
+echo Drive:                   %~d0%
+echo Path without drive:      %~p0%
+echo Drive and path:          %~dp0%
+echo Path with drive:         %~f0%
+echo Filename without suffix: %~n0%
+echo Suffix (incl. dot):      %~x0%
+echo Short name:              %~s0%
+echo Attributes:              %~a0%
+echo Last modification:       %~t0%
+echo File size:               %~z0%
