329b56909c5977d49d7ffe06e02a2e77ac8b822b
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-07-19 21:31:07 +0200

    + double-colons-problem.bat

---
+++ commands/rem/double-colons-problem.bat
@@ -0,0 +1,13 @@
+@echo off
+
+if exist double-colons-problem.bat (
+
+   rem Notify the user that double-colon-problem.bat was found
+   echo The file double-colons-problem.bat was found
+
+) else (
+
+   echo The file double-colons-problem.bat was not found
+
+   :: This «comment» causes the error «) was unexpected at this time."
+)
