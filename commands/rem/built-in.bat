ae3ca781793a484113d634d757dff030752846fb
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-07-20 05:01:23 +0200

    & -> &&

--- commands/rem/built-in.bat
+++ commands/rem/built-in.bat
@@ -1,2 +1,2 @@
 @rem /?
-@echo hello world & rem because rem is a command, in can be put in a command series with the ampersand
+@echo hello world && rem because rem is a command, in can be put in a command series with the ampersand
