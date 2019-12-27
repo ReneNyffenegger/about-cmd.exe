11afc9ce6f7344f4a05a8e1aa4100d0db6f7702e
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-12-27 21:03:10 +0100

    wild cards

--- commands/if/exist.bat
+++ commands/if/exist.bat
@@ -11,3 +11,15 @@ if exist foo.bar (
 ) else (
    echo The file does not exists
 )
+
+if exist *.bat (
+   echo There are .bat files in this directory
+) else (
+   echo No .bat files in this directory
+)
+
+if exist *.txt (
+   echo There are .txt files in this directory
+) else (
+   echo No .txt files in this directory
+)
