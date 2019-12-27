f4d083099731c4f4ef6eaf9c3e4b044e9f6169bc
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2019-12-27 21:11:59 +0100

    + exit with numerical value

---
+++ commands/exit/errorlevel/callee.bat
@@ -0,0 +1,5 @@
+echo This is a simple batch file
+echo that uses "exit /b 42" to return a
+echo a value to the caller
+
+exit /b 42
---
+++ commands/exit/errorlevel/caller.bat
@@ -0,0 +1,5 @@
+@echo off
+
+call callee.bat
+echo(
+echo callee.bat "returned" %errorlevel%
