f4c5228e678e033116e96e0254e01efce110620c
René Nyffenegger <rene.nyffenegger@adp-gmbh.ch>
2020-04-14 10:05:34 +0200

    + colored-output.bat

---
+++ commands/echo/colored-output.bat
@@ -0,0 +1,56 @@
+@ echo off
+
+rem
+rem  Forground colors
+rem
+
+echo [30m Black (dark)
+echo [31m Red (dark)
+echo [32m Green (dark)
+echo [33m Yellow (dark)
+echo [34m Blue (dark)
+echo [35m Magenta (dark)
+echo [36m Cyan (dark)
+echo [37m White (dark)
+
+echo [90m Black (light)
+echo [91m Red (light)
+echo [92m Green (light)
+echo [93m Yellow (light)
+echo [94m Blue (light)
+echo [95m Magenta (light)
+echo [96m Cyan (light)
+echo [97m White (light)
+
+rem
+rem  Background colors
+rem
+
+echo [40m Black (dark)
+echo [41m Red (dark)
+echo [42m Green (dark)
+echo [43m Yellow (dark)
+echo [44m Blue (dark)
+echo [45m Magenta (dark)
+echo [46m Cyan (dark)
+echo [47m White (dark)
+
+echo [100m Black (light)
+echo [101m Red (light)
+echo [102m Green (light)
+echo [103m Yellow (light)
+echo [104m Blue (light)
+echo [105m Magenta (light)
+echo [106m Cyan (light)
+echo [107m White (light)
+
+rem
+rem   Reset:
+rem
+echo [0m
+
+echo [1m Bold      [0m
+echo.
+echo [4m Underline [0m
+echo.
+echo [7m Inverse   [0m
